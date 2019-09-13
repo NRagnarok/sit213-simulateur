5f088bbc65b4bda170ccb445cf2b8ce2d3ad38a1
Nicolas Vuillermet <nicovuillermet@gmail.com>
2019-09-13 15:53:57 +0200

    Add Nico's Tests

---
+++ src/tests/Tests_Nico.java
@@ -0,0 +1,12 @@
+package tests;
+
+import sources.SourceFixe;
+import visualisations.SondeLogique;
+
+public class Tests_Nico {
+    public static void main(String[] args) {
+        SourceFixe sf;
+        SondeLogique sl1;
+
+    }
+}
