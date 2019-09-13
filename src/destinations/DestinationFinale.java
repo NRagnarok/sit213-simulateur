a1537856ad2e5ed2dc12df632bdd21ce7751338c
Nicolas Vuillermet <nicovuillermet@gmail.com>
2019-09-13 15:32:25 +0200

    Finalisation fonction DestinationFinale

--- src/destinations/DestinationFinale.java
+++ src/destinations/DestinationFinale.java
@@ -4,17 +4,8 @@ import information.Information;
 import information.InformationNonConforme;
 
 public class DestinationFinale extends Destination {
-    public DestinationFinale() {
-        super();
-    }
-
-    @Override
-    public Information getInformationRecue() {
-        return super.getInformationRecue();
-    }
-
     @Override
     public void recevoir(Information information) throws InformationNonConforme {
-
+        this.informationRecue = information;
     }
 }
