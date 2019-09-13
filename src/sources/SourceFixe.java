961115935440d028ab53b307dae57d02ded23ea3
Nicolas Vuillermet <nicovuillermet@gmail.com>
2019-09-13 14:55:55 +0200

    <(^0^ > )

---
+++ src/sources/SourceFixe.java
@@ -0,0 +1,37 @@
+package sources;
+
+import information.*;
+import destinations.DestinationInterface;
+import java.util.*;
+
+/**
+ * blablabla
+ * @author epoulb
+ */
+
+public class SourceFixe extends Source {
+
+    public SourceFixe() {
+        super();
+    }
+
+    @Override
+    public Information getInformationEmise() {
+        return super.getInformationEmise();
+    }
+
+    @Override
+    public void connecter(DestinationInterface destination) {
+        super.connecter(destination);
+    }
+
+    @Override
+    public void deconnecter(DestinationInterface destination) {
+        super.deconnecter(destination);
+    }
+
+    @Override
+    public void emettre() throws InformationNonConforme {
+        super.emettre();
+    }
+}
